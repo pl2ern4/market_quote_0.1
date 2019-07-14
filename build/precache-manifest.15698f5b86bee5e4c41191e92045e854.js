a09ffcd1e4bd3dab7e5ff3f2ba0816f887108f37
prerna <chaturvediprerna03@gmail.com>
2019-07-14 18:35:15 +1200

    build

---
+++ build/precache-manifest.15698f5b86bee5e4c41191e92045e854.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f90f74d100f76c7b80ca6e9799a57d",
+    "url": "/market_quote_0.1/index.html"
+  },
+  {
+    "revision": "8df021633468b01a0823",
+    "url": "/market_quote_0.1/static/css/main.8fd1bf4c.chunk.css"
+  },
+  {
+    "revision": "28b93800811586f76a34",
+    "url": "/market_quote_0.1/static/js/2.f8483c48.chunk.js"
+  },
+  {
+    "revision": "8df021633468b01a0823",
+    "url": "/market_quote_0.1/static/js/main.f63d83fe.chunk.js"
+  },
+  {
+    "revision": "771252536401adb56474",
+    "url": "/market_quote_0.1/static/js/runtime~main.b1dd3638.js"
+  }
+]);
